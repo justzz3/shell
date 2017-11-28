bf5931b96a3792bbaad26f68ebf4a60c5f418320
Zhou Bai Shun <zhoubs@popcap.com>
2017-11-28 10:55:40 +0800

    docker

---
+++ docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt update
+sudo apt install -y docker-ce
+sudo usermod -aG docker $USER
+
+DOCKER_COMPOSE_VERSION=1.17.1
+sudo curl -kL https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
